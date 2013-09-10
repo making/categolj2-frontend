002c4f26f2b35fee44c3c9dfb789ca46a490d460
Toshiaki Maki <makingx@gmail.com>
2013-09-11 00:39:20 +0900

    added uikit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,27 @@
-console.log('This would be the main JS file.');
+$(function () {
+    var entriesTmpl = $('#entries-tmpl').html(),
+        entriesCompiled = Handlebars.compile(entriesTmpl),
+        $entries = $('#entries'),
+        recentlyPostsTmpl = $('#recently-posts-tmpl').html(),
+        recentlyPostsCompiled = Handlebars.compile(recentlyPostsTmpl),
+        $recentlyPosts = $('#recently-posts');
+
+
+    Handlebars.registerHelper('categoryLink', function (categories) {
+        var ret = [], categoriesBuf = [], sep = '::';
+        for (var i = 0; i < categories.length; i++) {
+            var category = categories[i];
+            categoriesBuf.push(Handlebars.Utils.escapeExpression(category.category_name));
+
+            ret.push('<a href="categories/' + categoriesBuf.join(sep) + '">'
+                + Handlebars.Utils.escapeExpression(category.category_name)
+                + '</a>');
+        }
+        return new Handlebars.SafeString(ret.join(sep));
+    });
+
+    $.getJSON('https://s3-ap-northeast-1.amazonaws.com/dummyapi/entries.json').success(function (data) {
+        $entries.html(entriesCompiled(data));
+        $recentlyPosts.html(recentlyPostsCompiled(data));
+    });
+});
